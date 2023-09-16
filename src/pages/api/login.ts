10a2e56699e3d71947313b07ceeda68090e1c7a3
Konstantin Timoshuk <k.timoshuk@digitalnative.ru>
2023-09-16 02:37:45 +0200

    log
    Signed-off-by: Konstantin Timoshuk <k.timoshuk@digitalnative.ru>

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,42 +1,41 @@
-import UserModel from '../../models/User'
-import bcrypt from 'bcrypt';
-import dbConnect from '../../lib/mongoose';
+import UserModel from "../../models/User";
+import bcrypt from "bcrypt";
+import dbConnect from "../../lib/mongoose";
 
 export default async function handler(req: any, res: any) {
-    try {
-        if (req.method !== 'POST') {
-            res.status(405).send({ message: 'Only POST requests allowed' })
+  try {
+    if (req.method !== "POST") {
+      res.status(405).send({ message: "Only POST requests allowed" });
 
-            return
-        }
-
-        await dbConnect();
-
-        const body = JSON.parse(JSON.stringify(req.body))
-        const users = await UserModel.find({});
-
-        const user = users.find((user) => user.email === body.email);
-
-        
-        if (!user) {
-            res.status(404).send({ message: 'User does not exit!' })
-
-            return
-        }
-
-        await bcrypt.compare(req.body.password, user.password, (err, result) => {
-            if (result) {
-                res.status(200).json(user);
-
-                return
-            }
-
-            res.status(401).json({
-                error: 'Access denied'
-            });
-        })
-    } catch (error) {
-        res.status(405).send({ message: `${error}` })
-        return
+      return;
     }
-};
+
+    await dbConnect();
+
+    const body = JSON.parse(JSON.stringify(req.body));
+    const users = await UserModel.find({});
+    console.log(users);
+    const user = users.find((user) => user.email === body.email);
+
+    if (!user) {
+      res.status(404).send({ message: "User does not exit!" });
+
+      return;
+    }
+
+    await bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if (result) {
+        res.status(200).json(user);
+
+        return;
+      }
+
+      res.status(401).json({
+        error: "Access denied",
+      });
+    });
+  } catch (error) {
+    res.status(405).send({ message: `${error}` });
+    return;
+  }
+}
