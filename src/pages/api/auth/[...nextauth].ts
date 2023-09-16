629a1e7b99dcf940f51389c17d578d35b002498b
Konstantin Timoshuk <k.timoshuk@digitalnative.ru>
2023-09-16 02:34:57 +0200

    fix
    Signed-off-by: Konstantin Timoshuk <k.timoshuk@digitalnative.ru>

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,79 +1,79 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { baseApiUrl, baseUrl } from '@/config'
+import { baseApiUrl, baseUrl } from "@/config";
 
 export const authOptions = {
-    secret: 'SECRET',
-    // Configure one or more authentication providers
-    providers: [
-        CredentialsProvider({
-            // The name to display on the sign in form (e.g. "Sign in with...")
-            name: "Credentials",
-            credentials: {
-                email: {
-                    label: "Email",
-                    type: "text",
-                    placeholder: "Enter email",
-                },
-                password: {
-                    label: "Password",
-                    type: "password",
-                    placeholder: "Enter Password",
-                },
-            },
-
-            async authorize(credentials: any, req) {
-                const { email, password } = credentials
-
-                const res = await fetch(`${baseApiUrl}/login`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        email,
-                        password,
-                    }),
-                });
-
-                const user = await res.json();
-
-                if (res.ok && user) {
-                    return user;
-                }
-
-                return null;
-            },
-        }),
-    ],
-    callbacks: {
-        async session({ session, token }) {
-            session.user.role = token.role;
-            session.user.id = token.id;
-            session.accessToken = token.accessToken;
-
-            return session;
+  secret: "SECRET",
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "Enter email",
         },
-        async jwt({ token, user }) {
-            if (user) {
-                token.role = user.role;
-                token.accessToken = user.password;
-                token.id = user._id;
-            }
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter Password",
+        },
+      },
 
-            return token
-        },
-        async signIn({ user, account, profile, email, credentials }) {
-            return true
-        },
-        // async redirect({ url, baseUrl }) {
-        //     console.log(baseUrl, url, new URL(url).origin)
-        //     // Allows relative callback URLs
-        //     if (url.startsWith("/")) return `${baseUrl}${url}`
-        //     // Allows callback URLs on the same origin
-        //     else if (new URL(url).origin === baseUrl) return url
-        //     return baseUrl
-        // }
-    }
-}
-export default NextAuth(authOptions)
+      async authorize(credentials: any, req) {
+        const { email, password } = credentials;
+        console.log(credentials);
+        const res = await fetch(`${baseApiUrl}/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        });
+        const user = await res.json();
+        console.log(user);
+
+        if (res.ok && user) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      session.user.role = token.role;
+      session.user.id = token.id;
+      session.accessToken = token.accessToken;
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = user.role;
+        token.accessToken = user.password;
+        token.id = user._id;
+      }
+
+      return token;
+    },
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    // async redirect({ url, baseUrl }) {
+    //     console.log(baseUrl, url, new URL(url).origin)
+    //     // Allows relative callback URLs
+    //     if (url.startsWith("/")) return `${baseUrl}${url}`
+    //     // Allows callback URLs on the same origin
+    //     else if (new URL(url).origin === baseUrl) return url
+    //     return baseUrl
+    // }
+  },
+};
+export default NextAuth(authOptions);
