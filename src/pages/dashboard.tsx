814a123d4b9795671d7091c9716141d5c9012dad
GregoryTm <gtimoshuk@mtt.ru>
2023-11-18 17:18:09 +0300

    fix: added edits by comments

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,24 +1,28 @@
 import { getSession, signOut, useSession } from "next-auth/react";
 import { baseApiUrl, baseUrl } from "@/config";
 import Link from "next/link";
-
+type ObjType = {
+  ENGINEER: string;
+  CUSTOMER: string;
+  default: string;
+};
 export default function Dashboard(props) {
-  console.log(1);
+  const links: ObjType = {
+    ENGINEER: "/customers",
+    CUSTOMER: "/departments",
+    default: "/customers",
+  };
+
   const { data: session } = useSession();
   const user = session?.user;
   //@ts-ignore
-
-  function getUrlCustomers() {
+  function getUrlCustomers(): string {
     const role = session?.user?.role;
-    switch (role) {
-      case "CUSTOMER":
-        return "/departments";
-      case "ENGINEER":
-        return "/customers";
-      default:
-        return "/customers";
-    }
+
+    const keyRole: keyof ObjType = role || "default";
+    return links[keyRole];
   }
+
   return (
     <section className=" place-items-center">
       <Link className="text-blue-400 mb-2" href={getUrlCustomers()}>
@@ -26,21 +30,6 @@ export default function Dashboard(props) {
           Мониторинг показателей
         </div>
       </Link>
-      {/* <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Hello {session?.user?.name}
-        </h2>
-        <h1 className="dark:text-white">Users</h1>
-        {props.users?.length &&
-          props.users?.map((user) => {
-            return (
-              <li key={user.id} className="dark:text-white">
-                {user.name} {user.id}
-              </li>
-            );
-          })}
-        <br />
-      </div> */}
     </section>
   );
 }
