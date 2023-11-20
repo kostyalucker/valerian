1b94802f056d0744cb5c56493fed86a61f5869ef
GregoryTm <gtimoshuk@mtt.ru>
2023-11-20 17:54:22 +0300

    feat: added new form for list customers

--- src/pages/customers/index.jsx
+++ src/pages/customers/index.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useState } from "react";
 
 import { baseApiUrl } from "@/config";
-import { useRouter } from "next/router";
+import { useRouter, router } from "next/router";
 
 export default function CustomersPage({ customers, baseUrl }) {
   const [isLoading, setLoading] = useState(false);
@@ -32,6 +32,16 @@ export default function CustomersPage({ customers, baseUrl }) {
     }
   }
 
+  function openCustomer(e, id) {
+    e.preventDefault();
+    router.push(`/departments?userId=${id}`);
+  }
+
+  function editCustomer(e, id) {
+    e.preventDefault();
+    router.push(`/customers/${id}`);
+  }
+
   return (
     <>
       <p className="text-xl font-bold mb-4">Заказчики</p>
@@ -39,12 +49,14 @@ export default function CustomersPage({ customers, baseUrl }) {
         <div className="flex text-center mb-2">
           <div className="p-2 w-80 shrink-0">Имя</div>
           <div className="p-2 w-40 shrink-0">ИНН</div>
+          <div className="p-2 w-40 shrink-0"></div>
         </div>
-        {customersState?.map(({ _id, name, inn }) => (
-          <div key={_id}>
+        {/* {customersState?.map(({ _id, name, inn }) => (
+          <div className="flex items-stretch " key={_id}>
             <Link
               className="
                 block
+                flex
               "
               href={`/departments?userId=${_id}`}
             >
@@ -53,12 +65,70 @@ export default function CustomersPage({ customers, baseUrl }) {
                 <div className="p-2 border-2 w-40 shrink-0">{inn}</div>
               </div>
             </Link>
-            <button onClick={() => deleteCustomer(_id)} disabled={isLoading}>
-              delete user
-            </button>
-            <Link href={`/customers/${_id}`}>edit user</Link>
+            <div className="p-2 border-2 w-80 shrink-0">
+              <button onClick={() => deleteCustomer(_id)} disabled={isLoading}>
+                Удалить
+              </button>
+            </div>
+            <div className="p-2 border-2 w-80 shrink-0">
+              <Link href={`/customers/${_id}`}>редактировать</Link>
+            </div>
           </div>
-        ))}
+        ))} */}
+
+        <div class="relative overflow-x-auto">
+          <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" class="px-6 py-3">
+                  Имя
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Регион
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Город
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Инн
+                </th>
+                <th scope="col" class="px-6 py-3"></th>
+                <th scope="col" class="px-6 py-3"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {customersState?.map((customer) => (
+                <tr
+                  class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer  hover:bg-slate-100"
+                  key={customer._id}
+                  onClick={(e) => openCustomer(e, customer._id)}
+                >
+                  <td
+                    scope="row"
+                    class="px-6 py-4 font-medium whitespace-nowrap"
+                  >
+                    {customer.name}
+                  </td>
+                  <td class="px-6 py-4">{customer.region}</td>
+                  <td class="px-6 py-4">{customer.city}</td>
+                  <td class="px-6 py-4">{customer.inn}</td>
+                  <td
+                    class="px-6 py-4 bg-gray text hover:bg-slate-100"
+                    onClick={() => deleteCustomer(customer._id)}
+                  >
+                    <button disabled={isLoading}>Удалить</button>
+                  </td>
+                  <td
+                    class="px-6 py-4 bg-gray text hover:bg-slate-100"
+                    onClick={(e) => editCustomer(e, customer._id)}
+                  >
+                    Редактировать
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </ul>
     </>
   );
