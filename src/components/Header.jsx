88652e658653fd472a0290cd845a029882e5fd22
GregoryTm <gtimoshuk@mtt.ru>
2023-12-25 22:42:56 +0300

    fix: fix test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -64,7 +64,7 @@ function Header() {
                   }`}
                   href="/users/create"
                 >
-                  Создать пользователя
+                  Добавить заказчика
                 </Link>
               </>
             )}
